1922b7ca73dd4c266b362c2ba549cea6c093cb1f
deezertidal <jrull5400@gmail.com>
2022-07-08 23:52:18 +0800

    Update Auto-Gist.js

--- Auto-Gist.js
+++ Auto-Gist.js
@@ -1,1 +1,2 @@
 cron "30 6-23/3 * * *" script-path=https://raw.githubusercontent.com/Peng-YM/Sub-Store/master/backend/dist/cron-sync-artifacts.min.js, tag=Auto-Gist
+cron "15 7-18/2 * * *" script-path=https://raw.githubusercontent.com/xream/scripts/main/surge/modules/10010v3/10010.js, tag=流量监控, img-url=https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/mini/Cellular.png
