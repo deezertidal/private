20c0a19938d2de2e8188b9883432683fba0d5c8a
deezertidal <jrull5400@gmail.com>
2022-07-09 00:03:24 +0800

    Update js-sub.js

--- js-sub.js
+++ js-sub.js
@@ -1,3 +1,2 @@
 cron "30 6-23/3 * * *" script-path=https://raw.githubusercontent.com/Peng-YM/Sub-Store/master/backend/dist/cron-sync-artifacts.min.js, tag=Auto-Gist
 cron "15 7-18/2 * * *" script-path=https://raw.githubusercontent.com/xream/scripts/main/surge/modules/10010v3/10010.js, tag=流量监控, img-url=https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/mini/Cellular.png
-http-request ^https?:\/\/api\.tidal\.com\/v1\/(pages\/album|(user|track|page)s\/(\d+)\/(state|subscription|lyrics|onboarding|playbackinfopostpaywall)) script-path=https://raw.githubusercontent.com/yqc007/QuantumultX/master/TIDALHiFiPlusCrack.js, timeout=10, tag=Tidal HiFi
