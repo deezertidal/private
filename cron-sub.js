6015837e995905cc46fea4a71dd6d0f2b92b70cc
deezertidal <jrull5400@gmail.com>
2022-07-09 11:41:44 +0800

    Update cron-sub.js

--- cron-sub.js
+++ cron-sub.js
@@ -6,11 +6,9 @@ cron "30 6-23/3 * * *" script-path=https://raw.githubusercontent.com/Peng-YM/Sub
 # 联通免流监控
 cron "15 7-18/2 * * *" script-path=https://raw.githubusercontent.com/xream/scripts/main/surge/modules/10010v3/10010.js, tag=流量监控, img-url=https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/mini/
 
-
 # By Evilbutcher GitHub：https://github.com/evilbutcher/Quantumult_X/tree/master
 cron "45 4 8 * * *" script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/glados/checkin_env.js, enable=false, tag=机场签到
 cron "0 5 8 * * *" script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/glados/checkincookie_env.js, enable=false, tag=机场签到Cookie版
-
 
 # By NobyDa    GitHub：https://github.com/NobyDa/Script/tree/master
 cron "15 1 * * *" script-path=https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js, enable=false, tag=京东
@@ -21,8 +19,5 @@ cron "0 3 8 * * *" script-path=https://raw.githubusercontent.com/chavyleung/scri
 cron "20 3 8 * * *" script-path=https://raw.githubusercontent.com/chavyleung/scripts/master/v2ex/v2ex.js, enable=false, tag=V2EX
 cron "0 5 8 * * *" script-path=https://raw.githubusercontent.com/chavyleung/scripts/master/10010/10010.js, enable=false, tag=中国联通
 
-
-
 # photonmang  https://github.com/photonmang/quantumultX
 cron "10 0 8 * * *" script-path=https://raw.githubusercontent.com/photonmang/quantumultX/master/tuhu/tuhu.js, enable=false, tag=途虎养车
-
