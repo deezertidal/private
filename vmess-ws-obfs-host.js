2dd4df3057f188bff554bea2f8c07ee1dce60ff2
deezertidal <jrull5400@gmail.com>
2022-07-10 18:51:46 +0800

    Update vmess-ws-obfs-host.js

--- vmess-ws-obfs-host.js
+++ vmess-ws-obfs-host.js
@@ -1,6 +1,7 @@
 /**
  * 为 VMess WebSocket 节点修改混淆 host
  * https://raw.githubusercontent.com/Peng-YM/Sub-Store/master/scripts/vmess-ws-obfs-host.js#host=v.qq.com
+ * https://raw.githubusercontent.com/xream/scripts/main/surge/modules/sub-store-scripts/mega/index.js#title=免&host=v.qq.com&hostSuffix=[免流合集]&resolve=true
  */
 function operator(proxies) {
     const { host } = $arguments;
