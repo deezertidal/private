edab7f6a3220fb8fbcc1c9706e726eeb54993f6e
deezertidal <jrull5400@gmail.com>
2022-07-10 19:45:27 +0800

    Update vmess-host.js

--- vmess-host.js
+++ vmess-host.js
@@ -18,12 +18,12 @@ const network = getVal('network')
 /* network */
 const defaultNetworkPath = getVal('defaultNetworkPath') || '/'
 /* 节点名前缀 */
-const prefix = getVal('prefix') || ''
+const prefix = getVal('vmess host') || ''
 /* 节点名后缀 */
-const suffix = getVal('suffix') || ''
+const suffix = getVal('80/443') || ''
 /* 附加 Host 前缀 */
-const hostPrefix = getVal('vmess-host') || ''
-const hostSuffix = getVal('免流混淆') || ''
+const hostPrefix = getVal('hostPrefix') || ''
+const hostSuffix = getVal('hostSuffix') || ''
 /* 附加 Path 前缀 */
 const pathPrefix = getVal('pathPrefix') || ''
 const pathSuffix = getVal('pathSuffix') || ''
