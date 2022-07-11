45e656158c2e6378a307ba628448f64cacc6d5a6
deezertidal <jrull5400@gmail.com>
2022-07-11 14:39:34 +0800

    Update ws-filter.js

--- ws-filter.js
+++ ws-filter.js
@@ -1,8 +1,4 @@
 function filter(proxies) {
-  return proxies.forEach(p => {
-        if (p.type === 'vmess' && p.network === 'ws') {
-            p["ws-opts"] = p["ws-opts"] || {};
-            p["ws-opts"]["headers"] = p["ws-opts"]["headers"] || {};
-            p["ws-opts"]["headers"]["Host"] = host;
-        }
-    })
+  return proxies.(p => {
+        (p.type === 'vmess' && p.network === 'ws'));
+}
