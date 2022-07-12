79e6d50ecb2cb1254109f770c00d7f2cfc02beac
deezertidal <jrull5400@gmail.com>
2022-07-12 18:48:26 +0800

    Update nodes-filter.js

--- nodes-filter.js
+++ nodes-filter.js
@@ -3,7 +3,8 @@ function filter(proxies) {
   return proxies.map(p => [80,443].includes(p.port))*/
 function filter(proxies) {
   const allowedNetworks = ['ws'] // ['tcp', 'ws', 'h2', 'http', 'grpc']
+  const port = [80,443]
  return proxies.map(p => {
-  return p.type === "vmess" && allowedNetworks.includes(p.network)
+  return p.type === "vmess" && allowedNetworks.includes(p.network) && port.includes(p.port)
  });
 }
