47fa247864a0309a875515d9da135cdc2018ee26
deezertidal <jrull5400@gmail.com>
2022-07-12 08:23:14 +0800

    Update nodes-filter.js

--- nodes-filter.js
+++ nodes-filter.js
@@ -1,6 +1,6 @@
 //sub-store脚本操作，筛选出80，443端口
 
 function filter(proxies) {
-  return proxies.map(p => [80,443].includes(p.port)) && p => [ws].includes (p.network));
+  return proxies.map(p => [80,443].includes(p.port)) && p => 'ws'(p.network));
 	});
 }
