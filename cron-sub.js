e0249bd2396212d6382b7d109a6c7bb6b07a553e
deezertidal <jrull5400@gmail.com>
2022-07-13 11:04:25 +0800

    Update cron-sub.js

--- cron-sub.js
+++ cron-sub.js
@@ -1,7 +1,7 @@
 # 自用cron收集
 
 # 自动同步订阅配置
-cron "30 6-23/3 * * *" script-path=https://raw.githubusercontent.com/Peng-YM/Sub-Store/master/backend/dist/cron-sync-artifacts.min.js, tag=Auto-Gist
+cron "30 6-23/3 * * *" script-path=https://raw.githubusercontent.com/sub-store-org/Sub-Store/master/backend/dist/sub-store-parser.loon.min.js, tag=Auto-Gist
 
 # 联通免流监控
 cron "15 7-18/2 * * *" script-path=https://raw.githubusercontent.com/xream/scripts/main/surge/modules/10010v3/10010.js, tag=流量监控, img-url=https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/mini/
