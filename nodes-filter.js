30c309a7440b7f7a11ceda7a6f16f9c546277f11
deezertidal <jrull5400@gmail.com>
2022-07-14 14:20:40 +0800

    Update nodes-filter.js

--- nodes-filter.js
+++ nodes-filter.js
@@ -8,3 +8,8 @@ function filter(proxies) {
  });
 }
 */
+function filter(proxies) {
+	return proxies.map(p => {
+		return p.type === "vmess" && p.port === 80 && p.port === 443 && p.network === ws
+	});
+}
