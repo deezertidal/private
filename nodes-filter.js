78252412a193888ea911e3390939ec5233805ce1
deezertidal <jrull5400@gmail.com>
2022-07-14 15:27:21 +0800

    Update nodes-filter.js

--- nodes-filter.js
+++ nodes-filter.js
@@ -1,26 +1,4 @@
-/*
-function filter(proxies) {
-  return proxies.map(p => [80,443].includes(p.port))
-function filter(proxies) {
-  const allowedNetworks = ['ws'] // ['tcp', 'ws', 'h2', 'http', 'grpc']
- return proxies.map(p => {
-  return p.port === 80 && allowedNetworks.includes(p.network) 
- });
-}
-*/
-/*function filter(proxies) {
-	return proxies.map(p => {
-		return p.type === "vmess" && p.port === 80 && p.port === 443 && p.network === ws
-	});
-}*/
-/*function filter(proxies) {
-	return proxies.map(p => {
-		return p.type === "vmess" && p.network === "ws"
-	});
-*/
-
-
-# author:史提芬周
+/*脚本过滤筛选80,443，vmess,ws节点*/
 
 function filter(proxies) {
   const allowedNetworks = ['ws'] // ['tcp', 'ws', 'h2', 'http', 'grpc']
