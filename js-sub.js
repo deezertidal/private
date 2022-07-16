693c98a6247407140277748226ffa5a8ab7f71ac
deezertidal <jrull5400@gmail.com>
2022-07-16 09:46:24 +0800

    Update js-sub.js

--- js-sub.js
+++ js-sub.js
@@ -1,1 +1,2 @@
 http-request ^https?:\/\/api\.tidal\.com\/v1\/(pages\/album|(user|track|page)s\/(\d+)\/(state|subscription|lyrics|onboarding|playbackinfopostpaywall)) script-path=https://raw.githubusercontent.com/yqc007/QuantumultX/master/TIDALHiFiPlusCrack.js, timeout=10, tag=Tidal HiFi
+hostname = api.tidal.com
