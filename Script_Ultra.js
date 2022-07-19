6ceb48327bb9e5d917a4ee74adefb498d18180ee
deezertidal <jrull5400@gmail.com>
2022-07-19 21:12:24 +0800

    Update Script_Ultra.js

--- Script_Ultra.js
+++ Script_Ultra.js
@@ -1,4 +1,4 @@
-//全量脚本集
+// Modified by https://github.com/deezertidal
 
 cron "30 6-23/3 * * *" script-path=https://raw.githubusercontent.com/sub-store-org/Sub-Store/master/backend/dist/cron-sync-artifacts.min.js, tag=Auto-Gist
 cron "15 7-18/2 * * *" script-path=https://raw.githubusercontent.com/xream/scripts/main/surge/modules/10010v3/10010.js, tag=流量监控, img-url=https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/mini/
