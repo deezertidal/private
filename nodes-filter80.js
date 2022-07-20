76317cafa9bc0ac357de11016a36602a401d2c1e
deezertidal <jrull5400@gmail.com>
2022-07-20 15:03:54 +0800

    Create nodes-filter80.js

---
+++ nodes-filter80.js
@@ -0,0 +1,9 @@
+/*脚本过滤筛选80，vmess,ws节点*/
+
+function filter(proxies) {
+  const allowedNetworks = ['ws'] // ['tcp', 'ws', 'h2', 'http', 'grpc']
+  const port = [80,443]
+ return proxies.map(p => {
+  return p.type === "vmess" && allowedNetworks.includes(p.network) && port.includes(p.port)
+ });
+}
