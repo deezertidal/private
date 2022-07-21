b30c2d4d3005280a4a5ea6d5f691fe161ac91515
deezertidal <jrull5400@gmail.com>
2022-07-21 15:08:58 +0800

    Create ModeSwitcher.js

---
+++ ModeSwitcher.js
@@ -0,0 +1,12 @@
+var confStr = $config.getConfig()
+console.log(confStr)
+var conf = JSON.parse(confStr)
+if (conf.ssid) {
+    $config.setRunningModel(1)
+    $config.setSelectPolicy("节点选择","Auto")
+    $notification.post("🎈网络环境变化通知","切换为规则模式","专线节点")
+} else {
+    $config.setRunningModel(2)
+    $config.setSelectPolicy("节点选择","免流free")
+    $notification.post("🎈网络环境变化通知","切换为全局模式","免流节点")
+}
