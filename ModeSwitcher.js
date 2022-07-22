1ac01a4f3d80131b400162719168b8d0be2b6b0a
deezertidal <jrull5400@gmail.com>
2022-07-23 07:40:12 +0800

    Update ModeSwitcher.js

--- ModeSwitcher.js
+++ ModeSwitcher.js
@@ -1,12 +1,16 @@
+/*
+根据网络切换Loon运行模式
+  0:全局直连模式
+  1:规则分流模式
+  2:全局代理模式
+  */
 var confStr = $config.getConfig()
 console.log(confStr)
 var conf = JSON.parse(confStr)
 if (conf.ssid) {
     $config.setRunningModel(1)
-    //$config.setSelectPolicy("节点选择","Auto")
     $notification.post("Mode Switcher","🎈当前正在使用无线网络","🚦Loon切换为规则模式")
 } else {
     $config.setRunningModel(2)
-   //$config.setSelectPolicy("节点选择","免流free")
     $notification.post("Mode Switcher","🎈当前正在使用蜂窝数据","🚀Loon切换为全局模式")
 }
