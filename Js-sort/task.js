e1d519770990615cc27536cdaf05109c5617034f
deezertidal <jrull5400@gmail.com>
2022-07-25 11:18:42 +0800

    Update task.js

--- Js-sort/task.js
+++ Js-sort/task.js
@@ -1,4 +1,4 @@
-/*定时任务脚本*/
+//定时任务脚本，配合cookies.js使用
 
 cron "30 6-23/5 * * *" script-path=https://raw.githubusercontent.com/sub-store-org/Sub-Store/master/backend/dist/cron-sync-artifacts.min.js, tag=Auto-Gist
 cron "15 7-18/2 * * *" script-path=https://raw.githubusercontent.com/xream/scripts/main/surge/modules/10010v3/10010.js, tag=流量监控, img-url=https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/mini/
