279a3653edc47b3780fbc2089df27f81d073311f
deezertidal <jrull5400@gmail.com>
2022-08-07 15:53:09 +0800

    Update script_me.js

--- script_me.js
+++ script_me.js
@@ -2,4 +2,3 @@ cron "30 6-23/3 * * *" script-path=https://raw.githubusercontent.com/sub-store-o
 cron "15 7-18/2 * * *" script-path=https://raw.githubusercontent.com/xream/scripts/main/surge/modules/10010v3/10010.js, tag=流量监控, img-url=https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/mini/
 cron "30 0 8-22/2 * * *" script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/hotsearch/hot.js, timeout=600, tag=热门监控
 cron "5 0 * * *" script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/wechatsubs/wechatsubs.js, tag=公众号监控
-cron "10 8-22/2 * * *" script-path=https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tasks/caiyun.js,tag=彩云天气
