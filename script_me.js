6dfb44706fa632c1645dbbf152ddcea98693cfc3
deezertidal <jrull5400@gmail.com>
2022-08-11 13:23:02 +0800

    Update script_me.js

--- script_me.js
+++ script_me.js
@@ -1,2 +1,3 @@
-cron "30 6-23/3 * * *" script-path=https://raw.githubusercontent.com/sub-store-org/Sub-Store/master/backend/dist/cron-sync-artifacts.min.js, tag=Auto-Gist
-cron "15 7-18/2 * * *" script-path=https://raw.githubusercontent.com/xream/scripts/main/surge/modules/10010v3/10010.js, tag=流量监控, img-url=https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/mini/
+cron "30 6-23/3 * * *" script-path=https://raw.githubusercontent.com/sub-store-org/Sub-Store/master/backend/dist/cron-sync-artifacts.min.js, tag=Auto Gist
+cron "15 7-18/2 * * *" script-path=https://raw.githubusercontent.com/xream/scripts/main/surge/modules/10010v3/10010.js, tag=Traffic Monitor
+cron "0 0 10,14,16 * * ?" script-path=https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tasks/github.js,tag=Github Monitor
