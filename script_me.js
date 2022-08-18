35eb718e509fc16482920c86519b1a84fa1ab37b
deezertidal <jrull5400@gmail.com>
2022-08-18 19:08:08 +0800

    Update script_me.js

--- script_me.js
+++ script_me.js
@@ -1,2 +1,3 @@
 cron "30 6-23/3 * * *" script-path=https://raw.githubusercontent.com/sub-store-org/Sub-Store/master/backend/dist/cron-sync-artifacts.min.js, tag=Auto Gist
 cron "15 7-18/2 * * *" script-path=https://raw.githubusercontent.com/xream/scripts/main/surge/modules/10010v3/10010.js, tag=Traffic Monitor
+cron "10 7-22/2 * * *" script-path=https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tasks/caiyun.js,tag=Colorful Cloud
