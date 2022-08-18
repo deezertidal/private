5256a79422292c1b799a692672ab6e0a7fd6e0cd
deezertidal <jrull5400@gmail.com>
2022-08-18 16:36:32 +0800

    Update script_me.js

--- script_me.js
+++ script_me.js
@@ -1,2 +1,3 @@
 cron "30 6-23/3 * * *" script-path=https://raw.githubusercontent.com/sub-store-org/Sub-Store/master/backend/dist/cron-sync-artifacts.min.js, tag=Auto Gist
 cron "15 7-18/2 * * *" script-path=https://raw.githubusercontent.com/xream/scripts/main/surge/modules/10010v3/10010.js, tag=Traffic Monitor
+http-response ^https:\/\/spclient\.wg\.spotify\.com\/color-lyrics\/v2\/track\/ script-path=https://raw.githubusercontent.com/app2smile/rules/master/js/spotify-lyric.js, requires-body=true, binary-body-mode=true, timeout=10, tag=Spotify歌词翻译
