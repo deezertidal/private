b39b99b0a2f4208ecabfac6f868dce51336f93cb
deezertidal <jrull5400@gmail.com>
2023-07-08 22:20:08 +0800

    Update oil.js

--- oil.js
+++ oil.js
@@ -3,7 +3,7 @@ if (typeof $argument !== "undefined") {
   provname = $argument;
 } else {
 
-  provname = "江苏";//quantumultx用户请在定时任务界面 左滑编辑此脚本 更改其他省 省份名不能含"省"字。小火箭loon用户请看模块插件注释 surge请用面板
+  provname = "江苏";//quantumultx用户请复制到本地脚本 更改其他省 省份名不能含"省"字。小火箭loon用户请看模块插件注释 surge请用面板
 }
 //默认江苏油价
 const encodedprovname = encodeURIComponent(provname);
