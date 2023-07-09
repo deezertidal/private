16c2b3161478198c4290be80e3b08087bcffc260
deezertidal <jrull5400@gmail.com>
2023-07-09 18:38:56 +0800

    Update book.js

--- book.js
+++ book.js
@@ -1,51 +1,66 @@
-const url = "https://book.douban.com/latest?tag";
-const headers = {
+var url = "https://book.douban.com/latest?tag";
+var headers = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
 };
-const params = getParams($argument);
 
-$httpClient.get({ url: url, headers: headers }, function(error, response, data) {
-  if (error) {
-    console.log("请求失败:", error);
-    $done();
-    return;
-  }
-  handleResponse(data);
-});
+if (typeof $task !== 'undefined') {
+
+  $task.fetch({ url: url, headers: headers }).then(
+    function(response) {
+      handleResponse(response.body);
+    },
+    function(reason) {
+      console.log("请求失败:", reason.error);
+      $done();
+    }
+  );
+} else if (typeof $httpClient !== 'undefined') {
+
+  $httpClient.get({ url: url, headers: headers }, function(error, response, data) {
+    if (error) {
+      console.log("请求失败:", error);
+      $done();
+      return;
+    }
+    handleResponse(data);
+  });
+}
 
 function handleResponse(data) {
-  const bookData = extractBookData(data);
-  const books = bookData.slice(0, 9);
-  let panelContent = "";
-  for (let i = 0; i < books.length; i++) {
-    const book = books[i];
-    const title = "📖"+book.title;
-    const author = book.author;
-    const rating = book.rating || "暂无";
-    panelContent += `${i + 1}. ${title}\n作者:${author} 🌟:${rating}\n`;
+  var bookData = extractBookData(data);
+  var books = bookData.slice(0, 9);
+  var notificationContent = "";
+  for (var i = 0; i < books.length; i++) {
+    var book = books[i];
+    var title = book.title;
+    var author = book.author;
+    var rating = book.rating || "暂无";
+    notificationContent += ${i + 1}. ${title}\n作者:${author} 评分:${rating}\n;
   }
 
-  const panel = {
-    title: "新书速递",
-    content: panelContent,
-    icon: params.icon,
-    "icon-color": params.color
-  };
+  if (typeof $task !== 'undefined') {
 
-  $done(panel);
+    $notify("新书速递", "", notificationContent);
+  } else if (typeof $httpClient !== 'undefined') {
+
+    $notification.post("新书速递", "", notificationContent);
+  }
+  $done();
 }
 
 function extractBookData(html) {
-  const pattern = /<h2 class="clearfix">\s+<a class="fleft" href="([^"]+)">([^<]+)<\/a>\s+<\/h2>\s+<p class="subject-abstract color-gray">([^<]+)<\/p>\s+<p class="clearfix w250">[\s\S]*?<span class="font-small color-red fleft">([\d.]+)<\/span>/g;
-  let matches;
-  const books = [];
+  // ...
+  // ...
+  var pattern = /<h2 class="clearfix">\s+<a class="fleft" href="([^"]+)">([^<]+)<\/a>\s+<\/h2>\s+<p class="subject-abstract color-gray">([^<]+)<\/p>\s+<p class="clearfix w250">[\s\S]*?<span class="font-small color-red fleft">([\d.]+)<\/span>/g;
+  var matches;
+  var books = [];
   while ((matches = pattern.exec(html)) !== null) {
-    const link =matches[1];
-    const title = matches[2].replace(/^\d+\.\s/, "") ; 
-    const info = matches[3];
-    const rating = matches[4];
-    const authorMatch = info.match(/\s*(.+) \/ \d{4}-\d{1,2}/);
-    const author = authorMatch ? authorMatch[1] : "未知作者";
+    var link = matches[1];
+    var title = matches[2].replace(/^\d+\.\s/, "") + " 📖"; 
+    var info = matches[3];
+    var rating = matches[4];
+    var authorMatch = info.match(/\s*(.+) \/ \d{4}-\d{1,2}/);
+    var author = authorMatch ? authorMatch[1] : "未知作者";
     books.push({
       title: title,
       author: author,
@@ -53,13 +68,4 @@ function extractBookData(html) {
     });
   }
   return books;
-}
-
-function getParams(param) {
-  return Object.fromEntries(
-    $argument
-      .split("&")
-      .map((item) => item.split("="))
-      .map(([k, v]) => [k, decodeURIComponent(v)])
-  );
 }
