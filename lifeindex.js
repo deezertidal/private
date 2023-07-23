402c245e730976a40b30e1676b10e64c718832ae
deezertidal <jrull5400@gmail.com>
2023-07-23 10:08:44 +0800

    Update lifeindex.js

--- lifeindex.js
+++ lifeindex.js
@@ -1,5 +1,5 @@
 //const apiurl = "https://www.tianqiapi.com/life/lifepro?appid=79171417&appsecret=LP9yfUKd";
-const apiurl = "https://www.tianqiapi.com/life/lifepro?appid=83952424&appsecret=bg4NeEHO";
+const apiurl = "https://www.tianqiapi.com/life/lifepro?appid=77634847&appsecret=r1yT8ZId";
 
 const isQuantumultX = typeof $task !== "undefined";
 const isSurge = typeof $httpClient !== "undefined";
