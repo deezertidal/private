c48246bda4301b0d38e165274d11d84e5c3a3ac6
yfamily <jrull5400@gmail.com>
2023-10-23 18:39:04 +0800

    Update histoday.js

--- histoday.js
+++ histoday.js
@@ -1,9 +1,9 @@
-const url = "https://www.ipip5.com/today/api.php?type=json";
-
+const url = "https://lishishangdejintian.bmcx.com/";
 if (typeof $task !== "undefined") {
   $task.fetch({ url: url }).then(
     (response) => {
-      handleResponse(response.body);
+      const data = response.body.replace(/&nbsp;/g, ' ');
+      handleResponse(data);
     },
     (reason) => {
       console.log(reason.error);
@@ -11,40 +11,35 @@ if (typeof $task !== "undefined") {
     }
   );
 } else if (typeof $httpClient !== "undefined") {
-
   $httpClient.get(url, function (error, response, data) {
     if (error) {
       console.log(error);
       $done();
     } else {
-      handleResponse(data);
+      const sanitizedData = data.replace(/&nbsp;/g, ' ');
+      handleResponse(sanitizedData);
     }
   });
 }
 
 function handleResponse(data) {
-  const result = JSON.parse(data);
-  const today = result.today;
-  let events = result.result;
+  const regex = /(\d{4}年)(\d{1,2}月\d{1,2}日) <a href='\/\d+__lishishangdejintianchaxun\/' target='_blank'>(.*?)<\/a>/g;
+  const matches = [...data.matchAll(regex)];
 
-  events = events.filter((event) => event.year !== "2023");
+  if (matches.length > 0) {
+    const today = new Date().getFullYear();
+    const events = [];
 
-  events.sort((a, b) => parseInt(b.year) - parseInt(a.year));
-
-  const count = events.length > 9 ? 9 : events.length;
-  events = events.slice(0, count);
-
-  if (events.length > 0) {
-    const eventList = events.map((event) => `📓${event.year}年：${event.title}`).join("\n");
-    const notification = `${eventList}`;
+    for (const match of matches) {
+      events.push(`${match[1]} ${match[3]}`);
+    }
 
     if (typeof $task !== "undefined") {
-
-      $notify("历史上的" + today, "", notification);
+      $notify("历史上的今天", "", events.join("\n"));
     } else if (typeof $httpClient !== "undefined") {
-
-      $notification.post("历史上的" + today, "", notification);
+      $notification.post("历史上的今天" ,"", events.join("\n"));
     }
   }
+
   $done();
 }
